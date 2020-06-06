f249cf0302beb691ca5340783973ce79caa96171
nest2chuks <47100984+nest2chuks@users.noreply.github.com>
2020-06-06 05:30:40 +0100

    Update HNG-05373.py

--- scripts/HNG-05373.py
+++ scripts/HNG-05373.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-def hng_task2(Fullname, ID, P_Lang, E_Mail):
-
-    Fullname = "CHUKWUNTA, Ernest Chukwuebuka"
-    ID = "HNG-05375"
-    P_Lang = "Python"
-    E_Mail : "ernestchukwunta@yahoo.com"
-
-    print("Hello World, this is {Fullname} with HNGi7 ID {ID} using {P_Lang} for stage 2 task. {E_Mail}", flush = True)
-=======
-
-
 Fullname = "CHUKWUNTA, Ernest Chukwuebuka"
 ID = "HNG-05375"
 P_Lang = "Python"
